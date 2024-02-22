821cf246e1985f9b284b1ad01be2524aa5b52823
jbean <jab1896@msstate.edu>
2024-02-22 17:39:00 -0600

    test

--- client.cpp
+++ client.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
     if ((main_socket=socket(AF_INET, SOCK_DGRAM, 0))==-1) cout << "Error creating new socket." << endl;
     
     server.sin_port = htons(r_port);
-    //bcopy((char *)s->h_addr, (char *)&server.sin_addr.s_addr, s->h_length);  // test this
+    bcopy((char *)s->h_addr, (char *)&server.sin_addr.s_addr, s->h_length);  // test this
 
     ifstream file;
     file.open(filename);
