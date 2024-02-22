f174aa085c7e185265a512f8fbee8ad1d5d80dcc
jbean <jab1896@msstate.edu>
2024-02-22 10:13:57 -0600

    no pointer??

--- server.cpp
+++ server.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
     //cout << "Received data: " << payload << endl;
 
     int r_port = rand() % 64512 + 1024;
-    char* port_payload[512] = (to_string(r_port)).c_str();
+    char port_payload[512] = (to_string(r_port)).c_str();
 
     if (sendto(handshake_socket, port_payload, 64, 0, (struct sockaddr *)&client, clen)==-1) cout << "Error in sendto function." << endl;
   
