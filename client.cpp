44aaeae434445b048842b21ed09abd2dcc4d20e2
jbean <jab1896@msstate.edu>
2024-02-22 12:14:30 -0600

    testing

--- client.cpp
+++ client.cpp
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
         cout << "ack:" << ack << endl;
     }
 
-    payload[]= "DONE"
+    char payload[] = "DONE"
     if (sendto(main_socket, payload, 32, 0, (struct sockaddr *)&server, slen)==-1) cout << "Error in sendto function for file." << endl;
     //payload = EOF;
     //if (sendto(main_socket, payload, 4, 0, (struct sockaddr *)&server, slen)==-1) cout << "Error in sendto function for file." << endl;
